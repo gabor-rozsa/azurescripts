435457bb1520f8044e6fceb2221362afe0add407
Gabor ROZSA <66364710+gabor-rozsa@users.noreply.github.com>
2021-04-24 10:57:44 +0200

    Add files via upload

--- update-HCI-SDN-image.ps1
+++ update-HCI-SDN-image.ps1
@@ -7,12 +7,14 @@
 #   the Convert-WindowsImage.ps1 is faulty, remove the -PassThru parameter at every occurence of the Dismount-Diskimage command
 #   get the latest Azure Stack HCI OS updates from the Microsoft Update Catalog: https://www.catalog.update.microsoft.com/Search.aspx?q=azure%20stack%20hci
 
+$image = 'C:\Temp\SDN-VM.vhdx' #specify the SDN VM image location
+$updatesfolder = 'C:\Temp\updates' #location of the downloaded Azure HCI OS updates (latest Stack and CU)
+
 $volumes=(get-volume).driveletter #get the current driveletters
-mount-diskimage -imagepath C:\Temp\SDN-VM.vhdx #specify the SDN VM image location
+mount-diskimage -imagepath $image #mount the SDN VM image
 $newvolumes=(get-volume).driveletter  #get the driveletters after the VM image is mounted
 $mountvolume = $newvolumes | Where-Object {$volumes -notcontains $_} #get the driveletter of the mounted VM image
 
-$updatesfolder = 'C:\Temp\updates' #location of the downloaded Azure HCI OS updates (latest Stack and CU)
 Set-Location $updatesfolder
 
 $updates = get-childitem -Recurse | Where-Object { ($_.extension -eq ".msu") -or ($_.extension -eq ".cab") } | Select-Object fullname 
@@ -23,8 +25,7 @@ foreach ($update in $updates) {
     Invoke-Expression $command #execute the complied command(s)
 } 
 
-
-$command = "dism /image:G:\ /Cleanup-Image /spsuperseded" #comply the cleanup command
+$command = "dism /image:" + $mountvolume +":\ /Cleanup-Image /StartComponentCleanup /ResetBase" #comply the cleanup command
 Invoke-Expression $command #execute the complied command
 
-dismount-vhd -path C:\Temp\SDN-VM.vhdx -confirm:$false #dismount the VM file
+Dismount-DiskImage -imagepath $image -confirm:$false #dismount the VM file
